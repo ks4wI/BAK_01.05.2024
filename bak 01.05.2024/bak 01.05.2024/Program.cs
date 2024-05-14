0e11655a98089dffd98e40f67606ed515218f8f0
ks4wI <148214067+ks4wI@users.noreply.github.com>
2024-05-14 18:39:03 +0200

    Add files via upload

---
+++ bak 01.05.2024/bak 01.05.2024/Program.cs
@@ -0,0 +1,51 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj rozmiar listy: ");
+        int rozmiar = Convert.ToInt32(Console.ReadLine());
+
+        List<int> liczby = new List<int>(rozmiar);
+
+        Random random = new Random();
+        for (int i = 0; i < rozmiar; i++)
+        {
+            liczby.Add(random.Next(1, 100));
+        }
+
+        int dominant = znajdzDominante(liczby);
+
+        Console.WriteLine("Dominanta w liście: "+dominant);
+    }
+
+    static int znajdzDominante(List<int> liczby)
+    {
+        if (liczby == null || liczby.Count == 0)
+        {
+            throw new ArgumentException("Lista nie może być pusta.");
+        }
+
+        int maxCount = 0;
+        int dominanta = liczby[0];
+
+        foreach (int liczba in liczby)
+        {
+            int count = 0;
+            for (int i = 0; i < liczby.Count; i++)
+            {
+                if (liczby[i] == liczba)
+                {
+                    count++;
+                }
+            }
+
+            if (count > maxCount)
+            {
+                maxCount = count;
+                dominanta = liczba;
+            }
+        }
+
+        return dominanta;
+    }
+}
